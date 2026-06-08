cdf3d84916a13523cf1724be584a71fc6a359640
Chongqing Chen <45198455+RainyMoo@users.noreply.github.com>
2026-06-08 12:18:35 +0800

    Update result_eval.py

--- GEDSA-Net/datasets/vqa/eval/result_eval.py
+++ GEDSA-Net/datasets/vqa/eval/result_eval.py
@@ -1,5 +1,5 @@
-from openvqa.datasets.vqa.eval.vqa import VQA
-from openvqa.datasets.vqa.eval.vqaEval import VQAEval
+from GDESA-Net.datasets.vqa.eval.vqa import VQA
+from GDESA-Net.datasets.vqa.eval.vqaEval import VQAEval
 import json, pickle
 import numpy as np
 
